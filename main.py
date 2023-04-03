467fc027a32055c5499195414f6cb354d6a297f9
qas <simmaksimus@gmail.com>
2023-04-03 23:27:31 +0300

    Now the site has music

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from data import db_session
 from data.albums import Album
 from data.groups import Group
 from data.musicians import Musician
+from data.songs import Song
 
 app = Flask(__name__)
 # api = Api(app)
@@ -14,10 +15,9 @@ app.config['SECRET_KEY'] = 'the_freaking_key'
 def main():
     db_session.global_init("db/music.db")
     # db_sess = db_session.create_session()
-    # band = Group()
-    # band.name = ''
-    # band.genre = ''
-    # band.short_bio = ''
+    # band = Song()
+    # band.name = str(input())
+    # band.group_id = int(input())
     # db_sess.add(band)
     # db_sess.commit()
     # app.register_blueprint(news_api.blueprint)
@@ -29,6 +29,7 @@ def index():
     return render_template("infopage.html")
 
 
+# -------------------- списки --------------------
 @app.route("/groups")
 def group_list():
     db_sess = db_session.create_session()
@@ -41,8 +42,10 @@ def author_list():
     db_sess = db_session.create_session()
     authors = [mus for mus in db_sess.query(Musician).all()]
     return render_template("musician_page.html", authors=authors, db=db_sess, Group=Group)
+# -------------------- списки --------------------
 
 
+# -------------------- конкретные вещи --------------------
 @app.route("/group/<id>")
 def band_page(id):
     db_sess = db_session.create_session()
@@ -60,6 +63,17 @@ def author_page(id):
     return render_template("single_musician.html", author=author, bands=bands)
 
 
+@app.route("/group/<id>/album/<aid>")
+def album_page(id, aid):
+    db_sess = db_session.create_session()
+    band = db_sess.query(Group).get(id)
+    album = db_sess.query(Album).get(aid)
+    songs = [track for track in db_sess.query(Song).filter(Song.album_id == aid)]
+    return render_template("single_album.html", album=album, band=band, songs=songs)
+# -------------------- конкретные вещи --------------------
+
+
+# -------------------- ошибки --------------------
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
@@ -68,6 +82,7 @@ def not_found(error):
 @app.errorhandler(400)
 def bad_request(_):
     return make_response(jsonify({'error': 'Bad Request'}), 400)
+# -------------------- ошибки --------------------
 
 
 if __name__ == '__main__':
