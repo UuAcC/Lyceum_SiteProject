50937fbb3ea0f6894e19b2e72e0640faf56086ca
qas <simmaksimus@gmail.com>
2023-04-13 21:35:04 +0300

    Need to chill

---
+++ forms/add_band.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+
+class BandAddForm(FlaskForm):
+    name = StringField('Название группы:', validators=[DataRequired()])
+    genre = StringField('Жанр:', validators=[DataRequired()])
+    created_date = StringField('Дата создания:', validators=[DataRequired()])
+    closed_date = StringField('Дата распада:')
+    short_bio = TextAreaField('Краткая биография:', validators=[DataRequired()])
+    submit = SubmitField('Подтвердить')
